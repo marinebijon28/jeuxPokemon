c505f33c0ce25a770b74bd101bbd513b2a1d2273
marine bijon <marine.b.58@hotmail.fr>
2019-02-13 17:39:04 +0100

    affichage du menu

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import pygame
+from pygame.locals import *
+
+white = 255, 255, 255
 
 #initialisation
-pygame.init
+pygame.init()
 
 
 #creation de la fenetre
 window = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Pokemon")
 
+#affichage du menu
+font = pygame.font.Font(None, 36)
+text = font.render("Nouvelle partie", True, white)
+textpos = text.get_rect()
+textpos.centerx = window.get_rect().centerx
+textpos.centery = window.get_rect().centery
+window.blit(text, textpos)
+
+window.blit(window, (0, 0))
+pygame.display.flip()
+
+#boucle infinie
 game_over = False
 
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            game_over = True
+                game_over = True
 pygame.quit()
 quit()
